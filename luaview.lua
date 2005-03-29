d144c919a10893a6b8308a1e2b6ea9fe3e6a25c9
jeremy@abulafia <none@none>
2005-03-29 12:31:01 -0800

    bokchoi: add masking demo P4: 2185

---
+++ luaview.lua
@@ -0,0 +1,23 @@
+-- Example with no feature tracking
+
+require("bokstd")
+
+lua = gfx.texture('lua-logo.png')
+
+history={}
+table.setn(history, 4)		--number of frames of history
+
+function process_frame(frame)
+   table.insert(history, frame)	--insert at end
+   table.remove(history, 1)	--remove from start
+
+   --show old frame as background
+   gfx.setstate({colour={}, blend='none'})
+   if history[1] then
+      local prev = history[1]
+   end
+
+   --show current frame masked by Lua logo
+   gfx.setstate({blend='alpha'})
+   gfx.sprite(frame.width/2, frame.height/2, nil, frame, lua)
+end
