efc3b80e179a847a5ad666b86f0a22107964ffa6
jeremy@abulafia <none@none>
2005-05-14 00:11:44 -0700

    bokchoi/barney: added triangles P4: 2205

--- barney.lua
+++ barney.lua
@@ -1,64 +1,41 @@
 require('bokstd')
 
-histsize=30
-
--- color transition; gimp "incandescent" gradient
-incandescent =  {
-   { .46,  { .365,  0,    0   } },
-   { .59,  { .73,  .01,   0   } },
-   { .676, { .863, .269, .097 } },
-   { .809, { 1,    .545, .196 } },
-   { .853, { .986, .743, .135 } },
-   { .9,   { .973, .938, .081 } },
-   { .947, { .974, .953, .450 } },
-   { 1,    { .976, .968, .822 } },
-}
-
 blob = gfx.texture('blob.png')
 
-t = tracker.new(50, 200)
+--t = tracker.new(50, 200)
+t = tracker.new(20, 20)
 
 --metatable for points
-pointmeta = {}
-pointmeta.__index = pointmeta
-function pointmeta:lost(why)
+meta_point = {}
+meta_point.__index = meta_point
+function meta_point:lost(why)
    --print('lost',self,why)
-   if self.cluster then
-      self.cluster:remove(self)
-   end
+
+   triangles:del(self)
 end
 
-function pointmeta:draw()
+function meta_point:draw()
    gfx.setstate{colour=self.colour}
    gfx.sprite(self, 5, blob)
---[[
-   for i,p in self.history do
-      gfx.setstate{colour=gradient(incandescent, 1-(i/histsize))}
-      gfx.sprite(p, 5, blob)
-   end
-]]
 end
 
-function pointmeta:move(x, y)
+function meta_point:move(x, y)
    local dx,dy
    dx = self.x - x
    dy = self.y - y
    self.x,self.y = x,y
    self.dx = dx
    self.dy = dy
-
-   table.insert(self.history, 1, {x=x, y=y, dx=dx, dy=dy})
-   table.remove(self.history)
 end
 
-function pointmeta:update()
+function meta_point:update()
 end
 
-function pointmeta:__tostring()
+function meta_point:__tostring()
    return string.format('%g,%g', self.x, self.y)
 end
 
-function pointmeta.__lt(a,b)
+function meta_point.__lt(a,b)
    if a.y == b.y then
       if a.x < b.x then
 	 return a
@@ -77,18 +54,114 @@ end
 function features:add(idx, x, y, weight)
    pt = { x=x, y=y, key='pt'..unique() }
 
-   local h = {}
-   table.setn(h, histsize)
-   pt.history = h
+   pt.colour = { r=math.random(), g=math.random(), b=math.random() }
 
-   pt.colour = {r=math.random(), g=math.random(), b=math.random()}
+   pt.triangles = {}
 
-   setmetatable(pt, pointmeta)
+   setmetatable(pt, meta_point)
 
    self[idx] = pt
-   --print('add pt.__mesh=',pt.__mesh)
+
+   -- construct all the triangles with this point
+   for _,p1 in self:points() do
+      for _,p2 in self:points() do
+	 if pt ~= p1 and pt ~= p2 and p1 ~= p2 then
+	    triangles:new(p1, p2, pt)
+	 end
+      end
+   end
 end
 
+-- --
+
+-- Set of all existing triangles.  This table has several keys:
+--  - points -> triangles
+--  - triangle set
+triangles={}
+
+function angle(p1, p2)
+   local dx,dy = p1.x-p2.x, p1.y-p2.y
+
+   return math.atan2(dy, dx)
+end
+
+meta_triangle = {}
+meta_triangle.__index = meta_triangle
+
+-- Return a scalar representing how much the angles have changed since 
+-- the triangle was created
+function meta_triangle:diff()
+   local angles = {
+      angle(self[1], self[2]), 
+      angle(self[2], self[3]), 
+      angle(self[3], self[1]) }
+
+   local c1, c2, c3
+   c1 = math.cos(2*(angles[1] - self.angles[1]))
+   c2 = math.cos(2*(angles[2] - self.angles[2]))
+   c3 = math.cos(2*(angles[3] - self.angles[3]))
+
+   return c1*c2*c3
+end
+
+function meta_triangle:__tostring()
+   return string.format('(%s; %s; %s)', 
+			tostring(self[1]), 
+			tostring(self[2]), 
+			tostring(self[3]))
+end
+
+function meta_triangle:draw()
+   local a = self:diff()
+
+   if a < .9 then
+      gfx.setstate{color={1,0,0}}
+   else
+      gfx.setstate{color={1,1,0}}
+   end
+   gfx.line(self[1], self[2], self[3])
+end
+
+function meta_triangle:key()
+   return string.format('%s:%s:%s', self[1].key, self[2].key, self[3].key)
+end
+
+function triangles:new(p1, p2, p3)
+   local tri = { p1, p2, p3 }
+
+   setmetatable(tri, meta_triangle)
+
+   -- sort points into canonical form
+   table.sort(tri, function(p1, p2) return p1.key < p2.key end)
+
+   tri.angles = { 
+      angle(tri[1], tri[2]),
+      angle(tri[2], tri[3]), 
+      angle(tri[3], tri[1]) }
+
+   self[tri] = tri
+
+   for _,p in {p1,p2,p3} do
+      p[tri] = tri
+   end
+
+   p1.triangles[tri] = tri
+   p2.triangles[tri] = tri
+   p3.triangles[tri] = tri
+
+   --print(tri:key())
+end
+
+function triangles:del(p)
+   for t in p.triangles do
+      --print('for pt', p, 'matched', t)
+      assert(t[1] == p or t[2] == p or t[3] == p)
+      self[t] = nil
+   end
+   p.triangles={}
+end
+
+-- --
 function norm(v)
    return math.sqrt(v.dx*v.dx + v.dy*v.dy)
 end
@@ -200,18 +273,25 @@ end
 function process_frame(frame)
    --print('Frame!', frame)
 
+   gfx.setstate{colour={1,1,1,1}, blend='none'}
    t:track(features)
 
    drawframe(frame)
 
-
    gfx.setstate{colour={1,1,0,1}, blend='alpha'}
 
-   analyze(features:points())
+   --analyze(features:points())
 
-   if false then
+   if true then
       for _,p in features:points() do
 	 p:draw()
+      end
+
+      gfx.setstate{colour={1,1,0,1}, blend='none'}
+      for a,b in triangles do
+	 if a==b then
+	    a:draw()
+	 end
       end
    else
       local c
